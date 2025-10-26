cff672a00035f71d32b7fb348b7c220b367e22c7
Ardatik <belogurovigor9@gmail.com>
2025-10-26 20:34:46 +0300

    add groups

--- service/schedule/get_schedule.py
+++ service/schedule/get_schedule.py
@@ -2,7 +2,11 @@ from datetime import timedelta
 import os
 from service.request.request_data import request
 from dotenv import load_dotenv
-from service.schedule.schedule_processing import process_data_to_text_for_teachers, process_data_to_text_for_cabinets
+from service.schedule.schedule_processing import (
+    process_data_to_text_for_teachers,
+    process_data_to_text_for_cabinets,
+    process_data_to_text_for_groups
+)
 import logging
 
 logger = logging.getLogger(__name__)
@@ -10,7 +14,7 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 
 
-async def get_schedule(id, target_date, schedule_type='teachers'):
+async def get_schedule(id, target_date, schedule_type="teachers"):
     try:
         current_date = target_date
         start_of_current_week = current_date - timedelta(days=current_date.weekday())
@@ -21,22 +25,26 @@ async def get_schedule(id, target_date, schedule_type='teachers'):
         env_var_cab = os.getenv("GET_SCHEDULE_FOR_CABINETS")
         if not id:
             raise ValueError("ID is required")
-        if schedule_type == 'teachers':
+        if schedule_type == "teachers":
             request_url = f"{env_var_teach}?start_date={start_date_str}&end_date={end_date_str}&teacher={id}"
-        elif schedule_type == 'cabinet':
+        elif schedule_type == "cabinet":
             request_url = f"{env_var_cab}?start_date={start_date_str}&end_date={end_date_str}&place={id}"
+        elif schedule_type == "group":
+            request_url = f"{env_var_cab}?end_date={end_date_str}&group={id}&start_date={start_date_str}"
         else:
             raise ValueError(f"Unknown schedule type: {schedule_type}")
         print(f"url: {request_url}")
         data = await request(request_url)
         if isinstance(data, dict) and data.get("error"):
             raise Exception(f"API error: {data['error']}")
-        if schedule_type == 'teachers':
+        if schedule_type == "teachers":
             result = process_data_to_text_for_teachers(data, target_date)
-        elif schedule_type == 'cabinet':
+        elif schedule_type == "cabinet":
             result = process_data_to_text_for_cabinets(data, target_date)
+        elif schedule_type == "group":
+            result = process_data_to_text_for_groups(data, target_date)
         return result
-        
+
     except Exception as e:
         logger.exception(f"Ошибка получения расписания: {str(e)}")
         return f"Ошибка при получении расписания: {str(e)}"
--- service/schedule/schedule_processing.py
+++ service/schedule/schedule_processing.py
@@ -42,12 +42,6 @@ def process_data_to_text_for_cabinets(data, target_date):
             lesson_date_str = str(lesson.date)
             if lesson_date_str == target_date_str:
                 place_name = lesson.place.name if lesson.place else 'не указана'
-                teachers_names = []
-                for teacher in lesson.teachers:
-                    if hasattr(teacher, 'full_name'):
-                        teachers_names.append(teacher.full_name)
-                    elif isinstance(teacher, dict) and 'full_name' in teacher:
-                        teachers_names.append(teacher['full_name'])
                 start_time = lesson.start_time.replace('-', ':')[:5]
                 end_time = lesson.end_time.replace('-', ':')[:5]
                 text = (
@@ -55,6 +49,34 @@ def process_data_to_text_for_cabinets(data, target_date):
                     f"{lesson.type.short_name}, "
                     f"{lesson.discipline.short_name}, "
                     f"группы: {', '.join([group.name for group in lesson.groups])}, "
+                    f"преп: {', '.join([teacher.short_name for teacher in lesson.teachers])}, "
+                    f"ауд. {place_name}, "
+                    f"время: {start_time}:{end_time}"
+                )
+                schedule_list.append(text)
+    except Exception as e:
+        logging.error(f"Ошибка обработки расписания для кабинетов: {e}")
+        return f"Произошла ошибка при обработке расписания: {str(e)}"
+    if schedule_list:
+        return '\n'.join(schedule_list)
+    else:
+        return "На эту дату занятий не найдено"
+    
+def process_data_to_text_for_groups(data, target_date):
+    schedule_list = []
+    try:
+        schedule = Schedule.model_validate(data)
+        target_date_str = str(target_date)
+        for i, lesson in enumerate(schedule.data):
+            lesson_date_str = str(lesson.date)
+            if lesson_date_str == target_date_str:
+                place_name = lesson.place.name if lesson.place else 'не указана'
+                start_time = lesson.start_time.replace('-', ':')[:5]
+                end_time = lesson.end_time.replace('-', ':')[:5]
+                text = (
+                    f"{lesson.number}. "
+                    f"{lesson.type.short_name}, "
+                    f"{lesson.discipline.short_name}, "
                     f"преп: {', '.join([teacher.short_name for teacher in lesson.teachers])}, "
                     f"ауд. {place_name}, "
                     f"время: {start_time}:{end_time}"
@@ -75,4 +97,4 @@ async def out_readable_text(data, target_date, schedule_type):
     elif schedule_type == "teacher":
         return process_data_to_text_for_teachers(data, target_date)
     else:
-        pass
+        return process_data_to_text_for_groups(data, target_date)
